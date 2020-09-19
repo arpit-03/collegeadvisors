719a936914f10430aa3a498aaeb3aec9076c21d0
arpit-03 <arpitdeveloper03@gmail.com>
2020-09-19 16:53:57 +0530

    p2

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,14 +1,14 @@
 class ImageController < ApplicationController
  def show
- 	if (File.file?('/home/arpit/Desktop/scrapedu/artscolgdata/'+params[:name]))
-    send_file ('/home/arpit/Desktop/scrapedu/artscolgdata/'+params[:name].gsub('.',' ')), :disposition => 'inline'
+ 	if (File.file?('/allimages/'+params[:name]))
+    send_file ('/allimages/'+params[:name].gsub('.',' ')), :disposition => 'inline'
 else
 	k=params[:name].split('-')
 	puts k
 	if(k.last=="icon")
-	send_file File.join('/home/arpit/Desktop/scrapedu/artscolgdata/default-icon'), :disposition => 'inline'
+	send_file File.join('/allimages/default-icon'), :disposition => 'inline'
 else
-	send_file File.join('/home/arpit/Desktop/scrapedu/artscolgdata/default-cover'), :disposition => 'inline'
+	send_file File.join('/allimages/default-cover'), :disposition => 'inline'
 end
 end
  end
