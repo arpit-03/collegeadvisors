65c81ab9bcac3d457859f8350478565288c17dee
arpit-03 <arpitdeveloper03@gmail.com>
2020-09-20 09:32:44 +0530

    v3.18

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -152,12 +152,12 @@ else
 end
 end
 def slotsuccess
-  puts session[:user_id]
+
 if(Adminslot.find(params[:id]).user_id.to_s == session[:user_id])
 @slotinfo=Adminslot.find(params[:id])
-byebug
+
 else
-  byebug
+  
   redirect_to error_path
 end
 
